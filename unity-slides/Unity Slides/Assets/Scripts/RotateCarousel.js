d11f21b79a5d0fadeffd96526772fcfe4a458027
Colin MacDonald <cmacdonald@salesforce.com>
2015-06-15 17:30:16 -0300

    Project Start

---
+++ unity-slides/Unity Slides/Assets/Scripts/RotateCarousel.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+     
+public var amount : float = 90.0;
+public var speed : float = 5.0;
+
+private var destPosition : Vector3 = Vector3.zero;
+private var currPosition : Vector3;
+ 
+function Start() {
+	currPosition = destPosition;
+	transform.eulerAngles = destPosition;
+}
+ 
+function Update () {
+	if (Input.GetKeyDown(KeyCode.RightArrow) ) {
+		destPosition.y += -amount;
+	}
+
+	if (Input.GetKeyDown(KeyCode.LeftArrow) ) {
+		destPosition.y += amount;
+	}
+	 
+	 
+	currPosition = Vector3.Lerp(currPosition, destPosition, Time.deltaTime * speed);
+	transform.eulerAngles = currPosition;
+}
---
+++ unity-slides/Unity Slides/Assets/Scripts/SlideTransitioner.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+public var currentIndex : int = 0;
+private var previousIndex : int = currentIndex;
+private var currentSlide : GameObject;
+private var slides : Array = new Array();
+
+function Awake () {
+	// Store a ref to all the slides
+	for(var child : Transform in transform) {
+		child.gameObject.SetActive(false);
+		slides.push(child.gameObject);
+	}
+	
+	// Activate the first slide
+	currentSlide = slides[currentIndex];
+	currentSlide.SetActive(true);
+}
+
+function Update () {
+	if (Input.GetKey(KeyCode.RightArrow)) {
+		previousIndex = currentIndex;
+		currentIndex += 1;
+	}
+	
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		previousIndex = currentIndex;
+		currentIndex -= 1;
+	}
+}
+
+function ActivateSlide () {
+	currentSlide.SetActive(false);
+	currentSlide = slides[currentIndex];
+	currentSlide.SetActive(true);
+}
